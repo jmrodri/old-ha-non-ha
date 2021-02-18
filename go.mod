d3059f4146698d60dbeb24d18327ae47c8439b9e
jesus m. rodriguez <jmrodri@gmail.com>
2021-02-18 09:59:39 -0500

    getting access to the HA features in 2 diff ways

--- go.mod
+++ go.mod
@@ -3,11 +3,13 @@ module github.com/jmrodri/ha-non-ha
 go 1.15
 
 require (
-	github.com/openshift/api v0.0.0-20210208192252-670ac3fc997c // indirect
+	github.com/openshift/api v0.0.0-20210208192252-670ac3fc997c
 	github.com/openshift/client-go v0.0.0-20201214125552-e615e336eb49
 	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
 	golang.org/x/oauth2 v0.0.0-20210216194517-16ff1888fd2e // indirect
 	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
-	k8s.io/apimachinery v0.20.1
-	k8s.io/client-go v0.20.1
+	k8s.io/apimachinery v0.20.3
+	k8s.io/client-go v0.20.3
+	k8s.io/kubectl v0.20.3
+	sigs.k8s.io/controller-runtime v0.8.2
 )
--- main.go
+++ main.go
@@ -6,18 +6,84 @@ import (
 	"os"
 	"time"
 
+	configv1 "github.com/openshift/api/config/v1"
 	configclient "github.com/openshift/client-go/config/clientset/versioned"
 	configinformer "github.com/openshift/client-go/config/informers/externalversions"
 	configv1lister "github.com/openshift/client-go/config/listers/config/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	restclient "k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
+	crclient "sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
 )
 
 var infrastructureLister configv1lister.InfrastructureLister
 
+var (
+	scheme = runtime.NewScheme()
+)
+
+func init() {
+	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+	utilruntime.Must(configv1.AddToScheme(scheme))
+}
+
+func GetInfraViaK8S(cc *restclient.Config) error {
+	// Create a new config client
+	configClient, err := configclient.NewForConfig(cc)
+	if err != nil {
+		return err
+	}
+	operatorConfigInformer := configinformer.NewSharedInformerFactoryWithOptions(configClient, 2*time.Second)
+	infrastructureLister = operatorConfigInformer.Config().V1().Infrastructures().Lister()
+	infraConfig, err := configClient.ConfigV1().Infrastructures().Get(context.Background(), "cluster", metav1.GetOptions{})
+	if err != nil {
+		return err
+	}
+
+	// fmt.Printf("%v\n", infraConfig)
+	fmt.Printf("%v\n", infraConfig.Status.ControlPlaneTopology)
+	fmt.Printf("%v\n", infraConfig.Status.InfrastructureTopology)
+	return nil
+}
+
+func GetInfraViaControllerRuntime(cc *restclient.Config) error {
+	rm, err := apiutil.NewDynamicRESTMapper(cc)
+	if err != nil {
+		return err
+	}
+
+	crClient, err := crclient.New(cc, crclient.Options{
+		Scheme: scheme,
+		Mapper: rm,
+	})
+	if err != nil {
+		return err
+	}
+
+	// Simple query
+	nn := types.NamespacedName{
+		Name: "cluster",
+	}
+	infraConfig := &configv1.Infrastructure{}
+	err = crClient.Get(context.Background(), nn, infraConfig)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("using crclient: %v\n", infraConfig.Status.ControlPlaneTopology)
+	fmt.Printf("using crclient: %v\n", infraConfig.Status.InfrastructureTopology)
+
+	return nil
+}
+
 func main() {
 	fmt.Println("entered main")
 
+	// Load the config
 	loadingRules := clientcmd.NewDefaultClientConfigLoadingRules()
 	mergedConfig, err := loadingRules.Load()
 	if err != nil {
@@ -26,7 +92,6 @@ func main() {
 	}
 	fmt.Printf("clusters: %v\n", mergedConfig.Clusters)
 
-	fmt.Println("creating newdefaultclientconfig")
 	cfg := clientcmd.NewDefaultClientConfig(*mergedConfig, nil)
 	cc, err := cfg.ClientConfig()
 	if err != nil {
@@ -34,24 +99,17 @@ func main() {
 		os.Exit(-1)
 	}
 
-	fmt.Println("creating new auth operator client")
-
-	configClient, err := configclient.NewForConfig(cc)
+	// Get the Infra using straight K8S
+	err = GetInfraViaK8S(cc)
 	if err != nil {
-		fmt.Printf("problem creating new client from config: %v\n", err)
-		os.Exit(-2)
-	}
-
-	operatorConfigInformer := configinformer.NewSharedInformerFactoryWithOptions(configClient, 2*time.Second)
-	// operatorConfigInformer.Start(nil)
-	infrastructureLister = operatorConfigInformer.Config().V1().Infrastructures().Lister()
-
-	// infraConfig, err := infrastructureLister.Get("cluster")
-	infraConfig, err := configClient.ConfigV1().Infrastructures().Get(context.Background(), "cluster", metav1.GetOptions{})
-	if err != nil {
-		fmt.Printf("problem getting infrastructure config: %v\n", err)
+		fmt.Printf("problem getting via straight k8s: %v\n", err)
 		os.Exit(-4)
 	}
 
-	fmt.Printf("%v\n", infraConfig)
+	// Get the Infra using controller runtime
+	err = GetInfraViaControllerRuntime(cc)
+	if err != nil {
+		fmt.Printf("problem getting via controller runtime: %v\n", err)
+		os.Exit(-2)
+	}
 }
